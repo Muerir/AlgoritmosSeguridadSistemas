b567188e03edf7bf1eb8cc1714c9a246b7ed946d
Diego Cordova <diego.cordova.do.prado@gmail.com>
2020-09-08 18:56:41 -0400

    se arreglo el vigenere

--- js/vigenere.js
+++ js/vigenere.js
@@ -1,97 +1,73 @@
-function cifrar() {
-    var SIZE_ALF = 26;
-    
-    var clave = document.getElementById("clave").value;
-    var size_clave = clave.length;
-    var mensaje = document.getElementById("mensaje").value;
-    var size_mensaje = mensaje.length;
-    
-    // Guardar alfabeto
-      var alfabeto = new Array();
-    for(i = 0; i < SIZE_ALF; i++){
-      alfabeto[i] = String.fromCharCode(65 + i);
-    }
-    
-    // Guardar valores de la clave
-    var valores_clave = new Array();
-    for(i = 0; i < size_clave; i++){
-      for(j = 0; j < SIZE_ALF; j++){
-        if(clave[i] == alfabeto[j]){
-            valores_clave[i] = j;
+var addEvent = (document.addEventListener) ? 
+    function (elm,ev,cb) {elm.addEventListener(ev,cb,false);} : 
+function (elm,ev,cb) {elm.attachEvent("on"+ev, function(e){cb.call(elm, e);});};
+
+addEvent(window, "load", function init() {
+    var formField = document.getElementById("encdecForm");
+    var inputs = formField.getElementsByTagName("input");
+    addEvent(formField, "submit", function (evt) {
+        (evt.preventDefault) ? evt.preventDefault() : evt.returnValue=false;
+        var values = DomController.getValues(inputs);
+        if (!values.flg) {DomController.setResult("( N/A )"); return false;}
+        var isEncrypt = DomController.checkRadio(inputs);
+        var ipLength = values.ip.length;
+        var keyLength = values.key.length;
+        while (ipLength > keyLength) {
+            values.key += values.key;
+            keyLength = values.key.length;
         }
-      }
-    }
-    
-    // Cifrar
-    var mensaje_cifrado = new Array();
-    for(i = 0; i < size_mensaje; i++){
-      var pos_letra;
-      for(j = 0; j < SIZE_ALF; j++){
-        if(mensaje[i] == alfabeto[j]){
-          pos_letra = j;
+        for (var s=0, _1=_2=_3=result=""; s<ipLength; s++) {
+            _1 = VigenereAlgorithm.assign(values.ip.charAt(s));
+            _2 = VigenereAlgorithm.assign(values.key.charAt(s));
+            _3 = VigenereAlgorithm[isEncrypt](_1, _2);
+
+            result += VigenereAlgorithm.assign(_3);
         }
-      }
-      mensaje_cifrado[i] = alfabeto[(valores_clave[i%size_clave] + pos_letra) % SIZE_ALF];
-    }
-    
-    // Escribir el resultado en el HTML
-    div = document.getElementById("resultado1");
-    var cadena = "";
-    for(i = 0; i < size_mensaje; i++){
-      cadena += mensaje_cifrado[i];
-    }
-    div.textContent = 'Mensaje cifrado: ' + cadena;
-  }
-  
-  function modNeg(n1, n2){
-    var mod = n1;
-    while(mod < 0){
-      mod += n2;
-    }
-    return mod;
-  }
-  
-  function descifrar() {
-    var SIZE_ALF = 26;
-    
-    var clave = document.getElementById("clave2").value;
-    var size_clave = clave.length;
-    var mensaje = document.getElementById("mensaje_cif").value;
-    var size_mensaje = mensaje.length;
-    
-    // Guardar alfabeto
-      var alfabeto = new Array();
-    for(i = 0; i < SIZE_ALF; i++){
-      alfabeto[i] = String.fromCharCode(65 + i);
-    }
-    
-    // Guardar valores de la clave
-    var valores_clave = new Array();
-    for(i = 0; i < size_clave; i++){
-      for(j = 0; j < SIZE_ALF; j++){
-        if(clave[i] == alfabeto[j]){
-            valores_clave[i] = j;
+        if (result == -1) {DomController.setResult("Error Occur"); return false;}
+        DomController.setResult(result);
+    });
+});
+
+var DomController = {
+    getValues : function (inputs) {
+        var values = {};
+        values.flg = true;
+        for (var i=0, l=inputs.length; i<l; i++) {
+            switch (inputs[i].id) {
+                case "ip" : values.ip = inputs[i].value.toLowerCase(); break;
+                case "key": values.key= inputs[i].value.toLowerCase(); break;
+            }
         }
-      }
-    }
-    
-    // Descifrar
-    var mensaje_descifrado = new Array();
-    for(i = 0; i < size_mensaje; i++){
-      var pos_letra;
-      for(j = 0; j < SIZE_ALF; j++){
-        if(mensaje[i] == alfabeto[j]){
-          pos_letra = j;
+        for (target in values) {
+            if (values[target] !== values.flg) {
+                if (values[target].match(/[^A-Z]/i) || values[target] == "") {
+                    values.flg = false;
+                }
+            }
         }
-      }
-      mensaje_descifrado[i] = alfabeto[modNeg((pos_letra - valores_clave[i%size_clave]), SIZE_ALF)];
+        return values;
+    },
+    checkRadio : function (inputs) {
+        for (var i=0, l=inputs.length; i<l; i++) {
+            if (inputs[i].type === "radio" && inputs[i].checked) {
+                return inputs[i].value;
+            }
+        }
+    },
+    setResult : function (str) {
+        document.getElementById("result").value = str;
     }
-    
-    // Escribir el resultado en el HTML
-    div = document.getElementById("resultado2");
-    var cadena = "";
-    for(i = 0; i < size_mensaje; i++){
-      cadena += mensaje_descifrado[i];
+}
+
+var VigenereAlgorithm = {
+    encrypt : function (source, key){
+        return (source + key) % 26;
+    },
+    decrypt : function (cipher, key){
+        return (cipher - key + 26) % 26;
+    },
+    assign : function (value) {
+        var abc = "abcdefghijklmnopqrstuvwxyz";
+        return (isFinite(value)) ? abc.charAt(+value) : abc.indexOf(value);
     }
-    div.textContent = 'Mensaje descifrado: ' + cadena;
-  }
+}
