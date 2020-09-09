cef45eef024d146a1c0848e610883ec181b5ea76
Sergio <si.prudencio@acad.ucb.edu.bo>
2020-09-08 21:31:26 -0400

    sustitucion

---
+++ js/sustitucion.js
@@ -0,0 +1,29 @@
+// --- Variables sacadas del html ---
+const desplazamiento = document.getElementById("desplazamiento");
+const texto = document.getElementById("texto");
+const textoCifrado = document.getElementById("cifrado");
+// --- #Variables sacadas del html ---
+
+function cifrado(){
+
+	const textoIngresado = texto.value;  // valor del texto ingresado
+	textoCifrado.value = textoIngresado.split('').map(c=>{
+		let mayus = (c === c.toUpperCase()) ? true : false;
+        let valorEntero = c.toLowerCase().charCodeAt(0);
+        
+		if(valorEntero >= 97 && valorEntero <= 122)
+			{
+				const valorDesplazamiento = parseInt(desplazamiento.value);
+				
+				if(valorEntero + valorDesplazamiento > 122)
+					valorEntero = 97 + (valorEntero - 122) + valorDesplazamiento - 1;
+				else
+					valorEntero = valorEntero + valorDesplazamiento;
+			}
+	
+			let cifrado = String.fromCharCode(valorEntero);
+			return mayus ? cifrado.toUpperCase() : cifrado;
+		}).join('');
+}
+texto.addEventListener("keyup",cifrado);
+desplazamiento.addEventListener("change", cifrado);
