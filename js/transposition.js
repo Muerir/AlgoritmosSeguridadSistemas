2714b30e82ea430caaf8d31368df3f697effc395
Martín Campuzano Flores <martin.campuzano.flores@gmail.com>
2020-09-09 13:38:15 -0400

    Transposicion cifra y descifra

--- js/transposition.js
+++ js/transposition.js
@@ -9,7 +9,7 @@ function makeArrays(textSpaceless,keyLength){
     for(var i=0; i<Math.ceil(textSpaceless.length/keyLength); i++) {
         lines[i] = [];
         for(var j=0; j<keyLength; j++) {
-            lines[i][j] = undefined;
+            lines[i][j] = String.fromCharCode(97+Math.floor(Math.random() * 26))
         }
     }
     var long = 0;
@@ -24,6 +24,16 @@ function makeArrays(textSpaceless,keyLength){
     }
     return lines;
 }
+function makeArraysDecipher(text,keyLength){
+    var words = text.toLowerCase().split(' ');
+    var copy = words;
+    var arrayi = new Array();
+
+    for(var word of words){
+        arrayi.push(word.split(''));
+    }
+    return arrayi;
+}
 
 function orderList(lines, key, textSpaceless){
     var order = [];
@@ -37,14 +47,10 @@ function orderList(lines, key, textSpaceless){
     }
 
     var min = order.indexOf(Math.min(...order));
-    console.log(order);
-    console.log(lines);
     for(var i = 0; i < key.length;i++){
         for(var j=0; j<Math.ceil(textSpaceless.length/key.length); j++){
-            console.log(min);
             if(lines[j][min] != undefined){
                 cipher += lines [j][min];
-                console.log(cipher);
             }
         }
         order[min] = 1000;
@@ -67,9 +73,52 @@ function transpositionCipher(text,key){
     var textSpaceless = text.replace(/\s/g, '').toLowerCase().replace(/[^A-Za-z]+/g, '');
     var keyLength = key.length;
     var lines = makeArrays(textSpaceless,keyLength);
-    var text = separateCipher(lines,key, textSpaceless);
-    return text;
+    var cipher = separateCipher(lines,key, textSpaceless);
+    return cipher;
     
 }
 
-console.log(transpositionCipher("amigos como estan", "hola"))
+function transpositionDecipher(text,key){
+    var textSpaceless = text.replace(/\s/g, '').toLowerCase().replace(/[^A-Za-z]+/g, '');
+    var keyLength = key.length;
+    var lines = makeArraysDecipher(text,keyLength);
+    var lined = makeArrays(textSpaceless,keyLength);
+    
+    
+
+    var order = [];
+
+    var cipher = "";
+
+    
+    for(var letter of key){
+        let val = letterValue(letter);
+        order.push(val);
+    }
+    
+    
+    var min = order.indexOf(Math.min(...order));
+    console.log(lined)
+    console.log(min)
+    for(var i = 0; i < keyLength;i++){
+        console.log(min)
+        for(var j=0; j<Math.ceil(textSpaceless.length/key.length); j++){
+                console.log(min, j);
+                lined[j][min] = lines[i][j].toString();;         
+        }
+        console.log(lined);
+        order[min] = 1000;
+        min = order.indexOf(Math.min(...order));
+    }
+    
+    for(var line of lined){
+        for(var lett of line){
+            cipher += lett;
+        }
+    }
+   return cipher;
+}
+
+console.log(transpositionCipher("WE ARE DISCOVERED. FLEE AT ONCE.","zebras"))
+console.log(transpositionDecipher(transpositionCipher("WE ARE DISCOVERED. FLEE AT ONCE.","zebras"), "zebras"))
+console.log(transpositionDecipher("gosq aomt icen msoa", "hola"));
